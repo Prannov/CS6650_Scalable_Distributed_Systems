1e334b51d5ed2b0bf3bd30a5eb13e31d2f7b5804
Eroniction14 <eroniction14112000@gmail.com>
2026-04-20 22:25:56 -0400

    Updated UI with animations, added CORS middleware to event-svc

--- sportspulse/event-svc/main.go
+++ sportspulse/event-svc/main.go
@@ -98,6 +98,19 @@ func ingestHandler(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -113,8 +126,10 @@ func main() {
 	mux.HandleFunc("/health", healthHandler)
 	mux.HandleFunc("/events", ingestHandler)
 
+	handler := corsMiddleware(mux)
+
 	log.Printf("event-svc starting on :%s", port)
-	if err := http.ListenAndServe(":"+port, mux); err != nil {
+	if err := http.ListenAndServe(":"+port, handler); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
